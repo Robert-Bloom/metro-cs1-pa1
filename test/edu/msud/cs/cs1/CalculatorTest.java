ca1c7309a9fa9c8298fbf14bcd67d33575a54d65
Ivo Georgiev <ivogeorg@gmail.com>
2017-10-15 23:42:33 -0600

    added junit 5 test stubs

--- test/edu/msud/cs/cs1/CalculatorTest.java
+++ test/edu/msud/cs/cs1/CalculatorTest.java
@@ -1,4 +1,10 @@
 package edu.msud.cs.cs1;
 
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class CalculatorTest {
+
+    @Test
+    void test() {}
 }
--- test/edu/msud/cs/cs1/PluralizerTest.java
+++ test/edu/msud/cs/cs1/PluralizerTest.java
@@ -1,4 +1,11 @@
 package edu.msud.cs.cs1;
 
+import jdk.internal.jline.internal.TestAccessible;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class PluralizerTest {
+
+    @Test
+    void test() {}
 }
