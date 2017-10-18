4734b6b439028e99bfd1e2e4c374180ffc9886e7
Ivo Georgiev <ivogeorg@gmail.com>
2017-10-17 18:50:13 -0600

    implementation skeletons

--- src/edu/msud/cs/cs1/Calculator.java
+++ src/edu/msud/cs/cs1/Calculator.java
@@ -1,4 +1,15 @@
 package edu.msud.cs.cs1;
 
 public class Calculator {
+    enum Operation {
+        ADD,
+        SUBTRACT,
+        MULTIPLY,
+        DIVIDE,
+        MODULO
+    }
+
+    Calculator(int left, int right, Operation oper) {}
+
+    public int result() { return 0; }
 }
--- src/edu/msud/cs/cs1/Pluralizer.java
+++ src/edu/msud/cs/cs1/Pluralizer.java
@@ -1,4 +1,21 @@
 package edu.msud.cs.cs1;
 
+import java.io.File;
+
 public class Pluralizer {
+    private int numWords;
+
+    Pluralizer(String word) {
+        numWords = 1;
+    }
+
+    Pluralizer(File file) {
+        // numWords from parsed file
+    }
+
+    public String[] pluralize() {
+        String[] plurals = new String[numWords];
+
+        return plurals;
+    }
 }
