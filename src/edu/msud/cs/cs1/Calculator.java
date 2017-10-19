8dad0de6bdccc480429b93c71ef7fb34e3310ad4
Ivo Georgiev <ivogeorg@gmail.com>
2017-10-18 20:15:52 -0600

    refining basic requirements

--- src/edu/msud/cs/cs1/Calculator.java
+++ src/edu/msud/cs/cs1/Calculator.java
@@ -9,7 +9,16 @@ public class Calculator {
         MODULO
     }
 
+    // Contrary to the prototypical assignment, this version
+    // will have to support:
+    //   - both integers and decimals (doubles), by overloading
+    //   - positive, negative, and zero values
+    //   - modulo ONLY for integers
+    //   - arbitrary white space between operands and operator, excluding new lines
+    //   - parentheses for negative numbers
+    //   - bonus: three operands w/ operator precedence
     Calculator(int left, int right, Operation oper) {}
+    Calculator(String expression) { /* to parse */}
 
     public int result() { return 0; }
 }
--- src/edu/msud/cs/cs1/Pluralizer.java
+++ src/edu/msud/cs/cs1/Pluralizer.java
@@ -5,6 +5,10 @@ import java.io.File;
 public class Pluralizer {
     private int numWords;
 
+    // In contrast from the prototypical version of the assignment
+    // the input will only be a word which might or might not be
+    // in singlular or plural form. (The original provides an extra
+    // input of an integer as a count.
     Pluralizer(String word) {
         numWords = 1;
     }
@@ -13,6 +17,7 @@ public class Pluralizer {
         // numWords from parsed file
     }
 
+    // The returned array should contain ONLY plurals.
     public String[] pluralize() {
         String[] plurals = new String[numWords];
 
